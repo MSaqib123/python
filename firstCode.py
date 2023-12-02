7dfd1bebc5409c3cb792d7b164e94442956b4bdd
MBoota <m43577535@gmail.com>
2023-12-02 20:55:23 +0500

    Pakistan

--- firstCode.py
+++ firstCode.py
@@ -1,1 +1,17 @@
 print("Pakistan")
+
+
+# -----------------------------------------
+number  = 25
+print("Program STart : " + number)
+
+
+if number == 25
+    number =+ 25
+else 
+   number = 2
+
+print("Program End " + number)
+
+
+    
