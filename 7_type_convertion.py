ab75b82cfc78ad0981529183bd30c0fa50fc58a2
MBoota <m43577535@gmail.com>
2023-12-07 01:36:27 +0500

    git commit

---
+++ 7_type_convertion.py
@@ -0,0 +1,19 @@
+# Implecet convertion
+a = 44
+b = 5.5
+# b = a*b
+b = a+b
+print(b,' type of b = ',type(b))
+
+# Explicet convertion
+x = 55
+y = 23.5
+print(y , ' type of y = ',type(x*y))
+
+age = input('Enter your age')
+print(age , ' age = ',type(int(age)))
+
+name = input('Enter your Name')
+print(name , ' name = ',type(str(name)))
+
+
---
+++ 8_if_else_elsif.py
@@ -0,0 +1,52 @@
+hmza_age = 8
+college_age = 18
+scholl_age = 10
+uni = 26
+
+if hmza_age >= scholl_age and hmza_age <= 17:
+    print("Hmza can tak admission in school")
+    
+elif hmza_age >= college_age and hmza_age <= 25 :
+    print("Hamza can go to college")
+    
+elif hmza_age >= uni and hmza_age <=  35:
+    print("Hamza can go to university")
+
+elif hmza_age >= 36 :
+    print("Hamzaa ghar me rest kraa")
+
+elif hmza_age < 10 :
+    print("Kg Scholl")
+    
+else:
+    print("Not allowed in college univerty , scholl")
+    
+    
+
+# hmza_age = 22
+# college_age = 18
+# scholl_age = 10
+# uni = 26
+
+# if hmza_age >= scholl_age and hmza_age <= 17:
+#     print("Hmza can tak admission in school")
+    
+# elif hmza_age >= college_age and hmza_age <= 25 :
+#     print("Hamza can go to college")
+    
+# elif hmza_age >= uni and hmza_age <=  35:
+#     print("Hamza can go to university")
+
+# elif hmza_age >= 36 :
+#     print("Hamzaa ghar me rest kraa")
+
+# elif hmza_age < 10 :
+#     print("Kg Scholl")
+    
+# else:
+#     print("Not allowed in college univerty , scholl")
+    
+    
+    
+    
+    
