cc1ba837ebff541cb2ccbf835c1306a0cf0ba1c6
MBoota <m43577535@gmail.com>
2023-12-11 23:20:52 +0500

    today working

---
+++ 10_function.py
@@ -0,0 +1,52 @@
+#------------ Function --------------
+def plus():
+    print("Addition")
+    print("Addition")
+    print("Addition")
+    print("Addition")
+    print("Addition")
+    print("Addition")
+    print("Addition")
+    print("Addition")
+    print("Addition")
+    
+plus()
+plus()
+
+
+#------------ with variable --------------
+def function_1():
+    age = 25
+    print(age)
+    
+function_1()
+function_1()
+
+#------------ with paramter --------------
+def function_2(age):
+    print(age)
+    
+function_2()
+
+#------------ with if condition --------------
+def function_3(age):
+    if(age == 5) :
+        print("Adolt")
+    elif(age == 17) :
+        print("tenager")
+    else :
+        print("xyz")
+    
+function_3(52)
+function_3(52)
+
+
+
+print("\n\n")
+#------------ with if condition --------------
+def funReturn(age):
+    age = age+5
+    return age
+
+print(funReturn(25))
+    
